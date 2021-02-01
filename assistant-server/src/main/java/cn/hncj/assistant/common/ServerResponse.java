1a333514675039236723dc4b8437917c4addf84f
Tanyiqu <1953649096@qq.com>
2021-02-01 22:33:09 +0800

    封装返回值

---
+++ assistant-server/src/main/java/cn/hncj/assistant/common/ServerResponse.java
@@ -0,0 +1,101 @@
+package cn.hncj.assistant.common;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.io.Serializable;
+
+enum ResponseCode {
+    SUCCESS(200),
+    ERROR(401);
+
+    private int code;
+
+    public int getCode() {
+        return code;
+    }
+
+    ResponseCode(int code) {
+        this.code = code;
+    }
+}
+
+/**
+ * 封装返回结果
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ServerResponse<T> implements Serializable {
+    private Integer code;
+    private String msg;
+    private T data = null;
+
+    ServerResponse() {
+    }
+
+
+    /**
+     * 创建一个只有消息的成功的response
+     *
+     * @param msg  消息
+     * @param data 数据
+     * @return ServerResponse
+     */
+    public static <T> ServerResponse<T> createSuccess(String msg, T data) {
+        ServerResponse<T> successResponse = new ServerResponse<T>();
+        successResponse.setCode(ResponseCode.SUCCESS.getCode());
+        successResponse.setMsg(msg);
+        successResponse.setData(data);
+        return successResponse;
+    }
+
+    /**
+     * 创建一个只有消息的成功的response
+     *
+     * @param msg 消息
+     * @return ServerResponse
+     */
+    public static <T> ServerResponse<T> createSuccess(String msg) {
+        ServerResponse<T> successResponse = new ServerResponse<T>();
+        successResponse.setCode(ResponseCode.SUCCESS.getCode());
+        successResponse.setMsg(msg);
+        successResponse.setData(null);
+        return successResponse;
+    }
+
+    /**
+     * 创建一个失败的response
+     *
+     * @param errorMsg 失败消息
+     * @return ServerResponse
+     */
+    public static <T> ServerResponse<T> createError(String errorMsg) {
+        ServerResponse<T> errorResponse = new ServerResponse<T>();
+        errorResponse.setCode(ResponseCode.ERROR.getCode());
+        errorResponse.setMsg(errorMsg);
+        return errorResponse;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
--- assistant-server/src/main/java/cn/hncj/assistant/controller/TestController.java
+++ assistant-server/src/main/java/cn/hncj/assistant/controller/TestController.java
@@ -1,20 +1,16 @@
 package cn.hncj.assistant.controller;
 
+import cn.hncj.assistant.common.ServerResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 
 @RestController
 public class TestController {
 
     @RequestMapping("/test")
-    public Object hello() {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("status", 200);
-        map.put("msg", "测试成功");
-        map.put("data", new String[]{"aaa", "bbb", "ccc"});
-        return map;
+    public ServerResponse<Object> hello() {
+
+        return ServerResponse.createSuccess("测试成功", new String[]{"aaa", "bbb", "cc"});
     }
 
 }
